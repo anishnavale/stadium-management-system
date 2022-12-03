352d8c2fe74b722818aa0d0c6335edcc080622d2
Anish Navale <anishnavale@gmail.com>
2022-12-02 21:12:09 -0500

    #56 Match MGMT

---
+++ src/7_Match_Management_Module.sql
@@ -0,0 +1,140 @@
+--Author: Aditya, Anish, Sweta, Sejal
+--Comments: Script to generate reports from SMS tables
+--          Note: Run this script logged in as 'APP_ADMIN' user for Oracle Autonomous Database
+
+select * from match;
+select * from ticket;
+
+PURGE RECYCLEBIN;
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+    is_true NUMBER;
+BEGIN
+    dbms_output.put_line('---------------------------');
+    dbms_output.put_line('CHECKING IF SEQUENCES RELATED TO MATCH TABLE ALREADY EXISTS');
+    dbms_output.put_line('---------------------------');
+    SELECT
+        COUNT(*)
+    INTO is_true
+    FROM
+        all_sequences
+    WHERE
+        sequence_name = 'SEQ_MAT';
+
+    IF is_true > 0 THEN
+        dbms_output.put_line('Table: SEQ_MAT Already exists, dropping SEQUENCE');
+        EXECUTE IMMEDIATE 'DROP SEQUENCE SEQ_MAT';
+    END IF;
+    
+    SELECT
+        COUNT(*)
+    INTO is_true
+    FROM
+        all_sequences
+    WHERE
+        sequence_name = 'SEQ_REF';
+
+    IF is_true > 0 THEN
+        dbms_output.put_line('Table: SEQ_REF Already exists, dropping SEQUENCE');
+        EXECUTE IMMEDIATE 'DROP SEQUENCE SEQ_REF';
+    END IF;
+    
+END;
+/
+
+CREATE SEQUENCE SEQ_MAT
+    INCREMENT BY 1
+    START WITH 1
+    NOMAXVALUE
+    MINVALUE 1 
+    NOCYCLE
+    ORDER;
+    
+CREATE SEQUENCE SEQ_REF
+    INCREMENT BY 1
+    START WITH 1
+    NOMAXVALUE
+    MINVALUE 1 
+    NOCYCLE
+    ORDER;
+    
+CREATE OR REPLACE PROCEDURE PROC_ADD_NEW_MATCH(
+    in_league_name VARCHAR,
+    in_team1 VARCHAR,
+    in_team2 VARCHAR,
+    in_start_time TIMESTAMP,
+    in_end_time TIMESTAMP,
+    in_match_active VARCHAR
+)
+IS
+  is_true NUMBER;
+BEGIN
+  SELECT count(*)
+  INTO is_true
+  FROM match
+  WHERE in_start_time between m_start_time and m_end_time or
+  in_end_time between m_start_time and m_end_time;
+
+  IF is_true > 0 THEN
+        dbms_output.put_line('Time Conflict');
+    ELSE
+    INSERT INTO match (match_id, league_name, team1, team2, m_start_time, m_end_time, match_active)
+    VALUES (match_table_seq.nextval,in_league_name,in_team1,in_team2,in_start_time,in_end_time,in_match_active);
+    commit;
+    END IF;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE PROC_MODIFY_MATCH(
+    in_match_id NUMBER,
+    in_league_name VARCHAR,
+    in_team1 VARCHAR,
+    in_team2 VARCHAR,
+    in_start_time TIMESTAMP,
+    in_end_time TIMESTAMP,
+    in_match_active VARCHAR
+)
+IS
+  is_true NUMBER;
+BEGIN
+  SELECT count(*)
+  INTO is_true
+  FROM match
+  WHERE in_start_time between m_start_time and m_end_time or
+  in_end_time between m_start_time and m_end_time
+  and in_start_time < in_end_time;
+
+  IF is_true > 0 THEN
+        dbms_output.put_line('Time Conflict');
+    ELSE
+    UPDATE MATCH SET
+    league_name = in_league_name, team1 = in_team1, team2 = in_team2, m_start_time = in_start_time, m_end_time = in_end_time, match_active = in_match_active
+    WHERE match_id = in_match_id;
+    commit;
+    END IF;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE PROC_INACTIVATE_MATCH(
+    in_match_id NUMBER,
+    in_reason VARCHAR
+)
+IS
+  is_true NUMBER;
+BEGIN
+  SELECT count(*)
+  INTO is_true
+  FROM match
+  WHERE in_start_time between m_start_time and m_end_time or
+  in_end_time between m_start_time and m_end_time;
+
+    UPDATE TICKET SET
+    RFD_ID = 5
+    WHERE MATCH_ID = in_match_id;
+    commit;
+END;
+/
+
+
