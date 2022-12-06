70cdef7ba2d2d4a35ba8e54178b68b1e4e504132
Anish Navale <anishnavale@gmail.com>
2022-12-06 14:46:36 -0500

    #59 minor changes

--- src/9_Game_Day_Module.sql
+++ src/9_Game_Day_Module.sql
@@ -12,6 +12,7 @@ CREATE OR REPLACE PROCEDURE PROC_VERIFY_TICKET(
 )
 IS
   is_true NUMBER;
+  is_true1 NUMBER;
   exp_NULL_VALUE exception;
 BEGIN
     
@@ -26,13 +27,18 @@ BEGIN
   inner join match m on t.match_id = m.match_id
   WHERE rfd_id is NULL and ticket_id = in_ticket_id
   and extract(hour from (m.m_start_time - systimestamp)) between -1 and 3;
+  
+  SELECT count(*) into is_true1
+  FROM verification
+  where in_ticket_id = ticket_id;
 
-  IF is_true != 1 THEN
+  IF is_true != 1 and is_true1 = 0 THEN
         dbms_output.put_line('Invalid Ticket');
     ELSE
     INSERT INTO verification (ticket_id, v_date_time)
     VALUES (in_ticket_id, in_date_time);
     commit;
+    dbms_output.put_line('Valid Ticket');
     END IF;
     
     exception
@@ -48,3 +54,4 @@ END;
 /
 
 
+execute PROC_VERIFY_TICKET(1,systimestamp);
