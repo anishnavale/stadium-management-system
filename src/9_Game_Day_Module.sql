752ca3ef04204334312de729a4f6ed9db7bd5f00
Anish Navale <anishnavale@gmail.com>
2022-12-08 16:01:58 -0500

    #59 Added Test Cases

--- src/9_Game_Day_Module.sql
+++ src/9_Game_Day_Module.sql
@@ -23,22 +23,21 @@ BEGIN
     
   SELECT count(*)
   INTO is_true
-  FROM ticket t
-  inner join match m on t.match_id = m.match_id
-  WHERE rfd_id is NULL and ticket_id = in_ticket_id
-  and extract(hour from (m.m_start_time - systimestamp)) between -1 and 3;
+  FROM APP_ADMIN.ticket t
+  inner join APP_ADMIN.match m on t.match_id = m.match_id
+  WHERE rfd_id is NULL and ticket_id = in_ticket_id and to_char(m.m_start_time, 'dd-mm-yy') = to_char(in_date_time, 'dd-mm-yy') and extract(hour from (m.m_start_time - in_date_time)) between -1 and 3;
   
   SELECT count(*) into is_true1
-  FROM verification
+  FROM APP_ADMIN.verification
   where in_ticket_id = ticket_id;
 
-  IF is_true != 1 and is_true1 = 0 THEN
+  IF is_true != 1 or is_true1 != 0 THEN
         dbms_output.put_line('Invalid Ticket');
     ELSE
-    INSERT INTO verification (ticket_id, v_date_time)
-    VALUES (in_ticket_id, in_date_time);
-    commit;
-    dbms_output.put_line('Valid Ticket');
+        INSERT INTO APP_ADMIN.verification (ticket_id, v_date_time)
+        VALUES (in_ticket_id, in_date_time);
+        commit;
+        dbms_output.put_line('Valid Ticket');
     END IF;
     
     exception
@@ -53,5 +52,18 @@ BEGIN
 END;
 /
 
+-- Test Cases
 
-execute PROC_VERIFY_TICKET(1,systimestamp);
+-- Correct Ticket, Wrong Timestamp
+execute PROC_VERIFY_TICKET(19,systimestamp);
+
+-- Non-Existing Ticket, Correct Timestamp
+execute PROC_VERIFY_TICKET(113,TO_DATE('2022-12-20 09:40:00','yyyy-mm-dd hh24:mi:ss'));
+
+-- Correct Ticket, Correct Timestamp
+execute PROC_VERIFY_TICKET(24,TO_DATE('2022-12-20 09:40:00','yyyy-mm-dd hh24:mi:ss'));
+
+-- Used Ticket, Correct Timestamp
+execute PROC_VERIFY_TICKET(24,TO_DATE('2022-12-20 09:40:00','yyyy-mm-dd hh24:mi:ss'));
+
+--delete from verification where ticket_id = 24;
